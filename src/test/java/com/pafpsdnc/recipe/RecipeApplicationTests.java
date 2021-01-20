3cd5f7748ed0dd31f149d22b3f85d3b0b459e8bb
Paul Aboulinc <paul.aboulinc@outlook.fr>
2021-01-20 16:21:28 +0100

    add JenkinsFile + change test file

--- src/test/java/com/pafpsdnc/recipe/RecipeApplicationTests.java
+++ src/test/java/com/pafpsdnc/recipe/RecipeApplicationTests.java
@@ -1,13 +1,19 @@
 package com.pafpsdnc.recipe;
 
+import com.pafpsdnc.recipe.Controller.HellowController;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.beans.factory.annotation.Autowired;
+
 @SpringBootTest
 class RecipeApplicationTests {
+	@Autowired
+	private HellowController controller;
 
 	@Test
 	void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
-
 }
