ee6a530bc70992c20ecf992928b4247ec825dd15
Paul Aboulinc <paul.aboulinc@outlook.fr>
2021-02-22 15:14:48 +0100

    try to fix https on swagger

--- src/main/java/com/pafpsdnc/recipe/RecipeApplication.java
+++ src/main/java/com/pafpsdnc/recipe/RecipeApplication.java
@@ -1,12 +1,28 @@
 package com.pafpsdnc.recipe;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RecipeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RecipeApplication.class, args);
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().addServersItem(new Server().url("https://myserver.com"))
+                .components(new Components().addSecuritySchemes("basicScheme",
+                        new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("basic")))
+                .info(new Info().title("SpringShop API").version("V0")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
     }
 }
