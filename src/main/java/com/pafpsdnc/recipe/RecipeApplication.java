73e39a0d85e3dc1eb742f7d6e4748ab972334749
Paul Aboulinc <paul.aboulinc@outlook.fr>
2021-02-22 14:55:29 +0100

    test to add https to swagger request URL

--- src/main/java/com/pafpsdnc/recipe/RecipeApplication.java
+++ src/main/java/com/pafpsdnc/recipe/RecipeApplication.java
@@ -1,7 +1,10 @@
 package com.pafpsdnc.recipe;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RecipeApplication {
@@ -10,4 +13,9 @@ public class RecipeApplication {
 		SpringApplication.run(RecipeApplication.class, args);
 	}
 
+	@Bean
+	public OpenAPI openAPI() {
+		return new OpenAPI()
+				.addServersItem(new Server().url("/"));
+	}
 }
