d7910545ab2ae05b9d60024a1b5a4f97e90e1d59
Paul Aboulinc <paul.aboulinc@outlook.fr>
2021-02-22 15:03:51 +0100

    test to add https to swagger request URL

--- src/main/java/com/pafpsdnc/recipe/RecipeApplication.java
+++ src/main/java/com/pafpsdnc/recipe/RecipeApplication.java
@@ -1,7 +1,11 @@
 package com.pafpsdnc.recipe;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -14,8 +18,20 @@ public class RecipeApplication {
 	}
 
 	@Bean
-	public OpenAPI openAPI() {
+	public OpenAPI springShopOpenAPI(
+			@Value("${info.app.version}") String appVersion,
+			@Value("${server.servlet.context-path}") String contextPath
+	) {
+		System.out.println(appVersion);
+		System.out.println(contextPath);
 		return new OpenAPI()
-				.addServersItem(new Server().url("/"));
+				.addServersItem(new Server().url(contextPath))
+				.info(new Info()
+						.title("Lala")
+						.description("Lala")
+						.version(appVersion))
+				.components(new Components()
+						.addSecuritySchemes("basicScheme",
+								new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("basic")));
 	}
 }
