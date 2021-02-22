1cf065e4984d3943faa2d05c3c883531e7e9087e
Paul Aboulinc <paul.aboulinc@outlook.fr>
2021-02-22 15:09:32 +0100

    etst pipeline

--- src/main/java/com/pafpsdnc/recipe/RecipeApplication.java
+++ src/main/java/com/pafpsdnc/recipe/RecipeApplication.java
@@ -1,37 +1,12 @@
 package com.pafpsdnc.recipe;
 
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RecipeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RecipeApplication.class, args);
-	}
-
-	@Bean
-	public OpenAPI springShopOpenAPI(
-			@Value("${info.app.version}") String appVersion,
-			@Value("${server.servlet.context-path}") String contextPath
-	) {
-		System.out.println(appVersion);
-		System.out.println(contextPath);
-		return new OpenAPI()
-				.addServersItem(new Server().url(contextPath))
-				.info(new Info()
-						.title("Lala")
-						.description("Lala")
-						.version(appVersion))
-				.components(new Components()
-						.addSecuritySchemes("basicScheme",
-								new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("basic")));
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeApplication.class, args);
+    }
 }
